88bf4a1518cd33a7592a3b7c07955101782a61a4
TobiasPfaffeneder <tobiaspfaffeneder@gmail.com>
2025-11-02 00:48:39 +0100

    potetial fix 3

--- src/project.v
+++ src/project.v
@@ -4,11 +4,7 @@
  */
 `default_nettype none
 
-// Vizualization
 `include "sevenseg_decoder.v"
-//`include "bcd_splitter.v"
-//`include "digit_selector.v"
-//`include "sevenseg_display_controller.v"
 
 module tt_um_dip_switch_game_TobiasPfaffeneder (
     input  wire [7:0] ui_in,     // Dedicated input pins
@@ -21,24 +17,24 @@ module tt_um_dip_switch_game_TobiasPfaffeneder (
     input  wire rst_n            // Active-low reset (unused)
 );
 
-    // Instantiate the modules
-    // sevenseg_display_controller sevenseg_display_controller_inst (
-    //     .clk(clk),
-    //     .value(ui_in[7:0]),
-    //     .seg(uo_out[6:0])
-    // );
+    // interne Verbindung
+    wire [6:0] seg_w;
 
+    // Decoder-Instanz
     sevenseg_decoder sevenseg_decoder_inst (
         .digit(ui_in[3:0]),
-        .seg(uo_out[6:0])
+        .seg(seg_w)
     );
 
-    // Set unused output bits to 0
-    assign uio_out      = 0;
-    assign uio_oe       = 0;
-    assign uo_out[7]    = 0;
+    // Ausgabe vollständig definiert (8 Bit)
+    assign uo_out = {1'b0, seg_w};
 
-    // Tie off unused inputs to avoid warnings
-    wire _unused = &{ui_in[7:4], uio_in, ena, clk rst_n};
+    // Unbenutzte Ports deterministisch binden
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
+
+    // Vermeidung von "floating" Inputs im GL-Test
+    wire [3:0] unused_ui_in = ui_in[7:4];
+    wire _unused_ok = &{unused_ui_in, uio_in, ena, clk, rst_n};
 
 endmodule
